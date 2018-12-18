021fe992d5679df26c2ca30cff611f81f8899df9
Anthony Pogliano <poglianoster@gmail.com>
2018-12-18 13:50:22 -0700

    Commit.

--- mvc/controller/controller.js
+++ mvc/controller/controller.js
@@ -1,3 +1,7 @@
 const model = require("../model/model.js")
 
+const getOneLocation = (req, res, next) => {
+  res.status(200).json(model.getOneLoc)
+}
+
 module.exports = {}
--- mvc/model/model.js
+++ mvc/model/model.js
@@ -1,3 +1,10 @@
-// let knex = require('../../knex.js')  //uncomment this when knex.js exists and is connected to databases
+let knex = require('../../knex.js') //uncomment this when knex.js exists and is connected to databases
 
-module.exports = {}//knex functions go in here
+const getOneLoc = knex('pickup_locations')
+  .select('locationName', 'streetAddress')
+  .where('id', req.params.id)
+  .then((data) => {
+    return data
+  })
+
+module.exports = {} //knex functions go in here
